6b50f6f759e11902deafeb052685e19477c05914
Chanto Umana <umanav@amazon.com>
2018-06-12 11:35:36 -0600

    make changes

--- src/main/java/com/avvarga/popaballoon/controllers/UserController.java
+++ src/main/java/com/avvarga/popaballoon/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.avvarga.popaballoon.controllers;
 
 import java.util.Random;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,10 +32,23 @@ public class UserController {
 	public String test() {
 		return "test.jsp";
 	}
+	
+	@RequestMapping("/validate")
+	public String validate(@RequestParam("login") String login, @RequestParam("code") String code, HttpServletRequest request) {
+		try {
+			request.login(login, code);
+		} catch (ServletException e) {
+			System.out.println("Errors");
+			return "redirect:/"; 
+		}
+		System.out.println("you have entered");
+		return "redirect:/";
+		
+	}
+	
 	//Generate a code 
 	@PostMapping("/test/generate")
 	public String genCode(@RequestParam("login") String login,  Model model) {
-		System.out.println("entered");
 		//Generates a Random String 
 		String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
         StringBuilder salt = new StringBuilder();
@@ -47,9 +63,10 @@ public class UserController {
         User nUser =  new User();
         nUser.setLogin(login);
         nUser.setCode(saltStr);
+        System.out.println(saltStr);
         //Saves the user to the DB
-        uServ.saveUser(nUser);
-        System.out.println("saved");
+        uServ.saveWithUserRole(nUser);
 		return "redirect:/test";
 	}
+	
 }
--- src/main/java/com/avvarga/popaballoon/services/UserService.java
+++ src/main/java/com/avvarga/popaballoon/services/UserService.java
@@ -1,19 +1,33 @@
 package com.avvarga.popaballoon.services;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.avvarga.popaballoon.models.User;
+import com.avvarga.popaballoon.repositories.RoleRepository;
 import com.avvarga.popaballoon.repositories.UserRepository;
 
 @Service
 public class UserService {
 	
 	private UserRepository uRepo;
-	public UserService (UserRepository uRepo) {
+	private RoleRepository roleRepository;
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	public UserService (UserRepository uRepo, RoleRepository roleRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 		this.uRepo = uRepo;
+		this.roleRepository = roleRepository;
 	}
-	public void saveUser(User user) {
-		uRepo.save(user);
-	}
-
+	public User findByUsername(String login) {
+    	return uRepo.findByLogin(login);
+    }
+	
+	public void saveWithUserRole(User user) {
+        user.setCode(bCryptPasswordEncoder.encode(user.getCode()));
+        user.setRoles(roleRepository.findByName("ROLE_USER"));
+        uRepo.save(user);
+    }
+	
+	
 }
